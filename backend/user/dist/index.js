cffd26048304ac5fade31f5bf17bdd179564ee4c
sumit <mazumdarsumit3@gmail.com>
2025-07-29 00:27:01 +0530

    chore: added redis connection

--- backend/user/dist/index.js
+++ backend/user/dist/index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./configs/db.config.js";
+import { createClient } from "redis";
 dotenv.config();
 connectDb();
+export const redisClient = createClient({
+    url: process.env.REDIS_URI
+});
+redisClient.on("error", (err) => {
+    throw (err);
+});
+await redisClient.connect()
+    .then(() => console.log("Connected to redis !!!"))
+    .catch(console.error);
 const app = express();
 const port = process.env.PORT;
 app.listen(port, () => {
